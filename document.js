0d43f391f9dbff40547656e3b4389fed2a7b7797
julio3514 <mrsilva3514@gmail.com>
2025-04-08 15:09:24 +0300

    Update document.js

--- document.js
+++ document.js
@@ -1,11 +1,1 @@
-document.getElementById('instagram-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = document.getElementById('email').value;
-    
-    if (email) {
-        alert('האימייל אומת בהצלחה! עכשיו תוכל לגשת לאינסטגרם.');
-        window.location.href = 'https://www.instagram.com/nave_b1936/';
-    } else {
-        alert('אנא הזן אימייל תקני לאימות.');
-    }
-});
+
